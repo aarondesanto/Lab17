e61f69e28fceedb210c22b490db541109cdeb5e6
adesanto <aarondesanto@gmail.com>
2015-11-19 11:26:12 -0500

    basic angular logic added

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-$(document).ready(function() {
+var app = angular.module('mainModule', ['ngRoute']);
 
+app.config(function($routeProvider){
+  $routeProvider
+    .when('/', {
+      templateUrl: 'newpage.html',
+      controller: 'mainControl'
+    })
+    .when('/view2', {
+      templateUrl: 'newpage2.html',
+      controller: 'mainControl'
+    })
+    .otherwise({ redirectTo: '/' });
+});
 
+app.controller('mainControl', function() {
 
 });
